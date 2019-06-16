888b3e5137063bbaf961ab86fe8634071fc80ae0
selfbot33 <51696927+selfbot33@users.noreply.github.com>
2019-06-17 01:08:51 +0200

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+module.exports = class {
+  constructor(client) {
+    this.client = client;
+  }
+
+  async run() {
+    await this.client.wait(1000);
+
+    this.client.appInfo = await this.client.fetchApplication();
+    setInterval(async () => {
+      this.client.appInfo = await this.client.fetchApplication();
+    }, 60000);
+
+    this.client.user.setActivity("with Alex");
+
+    const channel = this.client.channels.get("539467117939130398");
+    channel.send(":gear: Le bot est redémarré !");
+
+    this.client.logger.log(
+      `Flareon est prêt à espionner ${
+        this.client.users.size
+      } utilisateurs sur ${this.client.channels.size} salons.`,
+      "ready"
+    );
+  }
+};
